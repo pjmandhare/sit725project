9f80efe3da388ff185a4860736062781759e1671
pjmandhare <82952364+pjmandhare@users.noreply.github.com>
2021-04-26 14:33:56 +1000

    Project initialised
    Initial code of html, js and css

---
+++ script.js
@@ -0,0 +1,9 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
